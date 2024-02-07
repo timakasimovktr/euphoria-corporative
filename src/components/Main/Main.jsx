5ae35e93005a2ff1c457000e5b82fe218e59ee34
Timur Kasimov <106226856+timakasimovktr@users.noreply.github.com>
2024-02-07 20:22:49 +0500

    susannas changes

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -30,12 +30,21 @@ function Main() {
       </header>
       <main>
         <div className="main-wrapper container">
-          <div style={{ width: "55%" }}>
-            <h1>СОЗДАВАЙТЕ СВОЮ ЭЙФОРИЮ С НАМИ КАЖДЫЙ ДЕНЬ.</h1>
-            <p>Тут будет текст, который добавится позже</p>
-            <div className="card-wrapper"></div>
+          <div style={{ width: "55%" }} className="first-main-wrapper">
+            <div>
+              <h1>СОЗДАВАЙТЕ СВОЮ ЭЙФОРИЮ С НАМИ КАЖДЫЙ ДЕНЬ.</h1>
+              <p>Тут будет текст, который добавится позже</p>
+            </div>
+            <div className="card-wrapper">
+              <div className="card"></div>
+              <div className="card"></div>
+              <div className="card"></div>
+            </div>
           </div>
-          <div style={{ width: "35%" }}></div>
+          <div style={{ width: "40%" }}>
+            <div></div>
+            partner-program  
+          </div>
         </div>
       </main>
       <footer></footer>
